05ac0839726d5cb2ec051d0f49f8378d5e5dd415
cedriccreusot <cedric.creusot@consulting-for.accor.com>
2019-11-07 14:22:23 +0100

    2nd round with the team (just improve test title)

--- src/test/kotlin/KataLightSaberLeague/AppTest.kt
+++ src/test/kotlin/KataLightSaberLeague/AppTest.kt
@@ -3,31 +3,26 @@
  */
 package KataLightSaberLeague
 
-import kotlin.test.Test
-import kotlin.test.assertNotNull
+import io.kotlintest.shouldThrow
+import io.kotlintest.specs.StringSpec
+import kotlin.test.assertNull
 
-class AppTest {
-    @Test fun testAppHasAGreeting() {
-        val classUnderTest = App()
-        assertNotNull(classUnderTest.greeting, "app should have a greeting")
+class AppTest : StringSpec({
+    "get 3 best players throw no best player found" {
+        shouldThrow<NoBestPlayersFoundException> {
+            val clan = Clan("Helico", listOf())
+
+            clan.get3BestPlayers()
+        }
     }
 
-    @Test(expected = NoBestPlayersFoundException::class)
-    fun `get 3 best players throw no best player found`() {
-        val clan = Clan("Helico", listOf())
-
-        clan.get3BestPlayers()
-    }
-
-    @Test
-    fun `get 3 best players return minimum 1 player`() {
+    "get 3 best players return minimum 1 player" {
         val clan = Clan("Helico", listOf(Player("Robin", 0)))
 
         assert(clan.get3BestPlayers().isNotEmpty())
     }
 
-    @Test
-    fun `get 3 best players return maximum 3 players`() {
+    "get 3 best players return maximum 3 players" {
         val clan = Clan(
             "Helico", listOf(
                 Player("Robin", 0), Player("Erika", 0),
@@ -38,8 +33,7 @@ class AppTest {
         assert(clan.get3BestPlayers().size < 4)
     }
 
-    @Test
-    fun `get 3 best players return 3 differents players`() {
+    "get 3 best players return 2 differents players" {
         val player = Player("Robin", 0)
         val players = listOf(
             player, player,
@@ -52,19 +46,50 @@ class AppTest {
         assert(clan.get3BestPlayers().size == 2)
     }
 
-    @Test
-    fun `get 3 best players after a fight the winner should be above looser`() {
+    "winner should be above looser after a fight when winner is below looser" {
         val winner = Player("Erika", 0)
+        val looser = Player("Robin", 0)
         val clan = Clan(
             "Helico", listOf(
-                Player("Robin", 0), Player("Christelle", 0),
+                looser, Player("Christelle", 0),
                 winner, Player("Le Fantome", 0)
             )
         )
-
+        val battle = InnerClanBattle()
+        val winnerPlayer = battle.fight(winner, looser)
+        clan.registerFightScore(winnerPlayer!!, looser)
         assert(clan.get3BestPlayers()[0] == winner)
+        assert(clan.get3BestPlayers()[1] == looser)
     }
-}
+
+    "if winner is above looser then it should do nothing" {
+        val winner = Player("Erika", 0)
+        val looser = Player("Robin", 0)
+        val clan = Clan(
+            "versateam", listOf(
+                winner, Player("Didi", 100),
+                looser, Player("Mon Seigneur", 0)
+            )
+        )
+        val battle = InnerClanBattle()
+        val winnerPlayer = battle.fight(winner, looser)
+        clan.registerFightScore(winnerPlayer!!, looser)
+        assert(clan.get3BestPlayers()[0] == winner)
+        assert(clan.get3BestPlayers()[2] == looser)
+    }
+
+    "if player is fighting himself then should do nothing" {
+        val player = Player("Erika", 0)
+        val clanErika = Clan("erika", listOf(player))
+        val battle = InnerClanBattle()
+        val noWinner = battle.fight(player, player)
+
+        clanErika.registerFightScore(noWinner, noWinner)
+
+        assertNull(noWinner)
+        assert(clanErika.get3BestPlayers()[0] == player)
+    }
+})
 
 class NoBestPlayersFoundException : Exception()
 
@@ -87,11 +112,26 @@ class Clan(val name: String, players: List<Player>) {
         }
     }
 
-    fun registerFightScore(winner: Player, looser: Player) {
+    fun registerFightScore(winner: Player?, looser: Player?) {
+        if (winner == null || looser == null)
+            return
 
+        if (players.indexOf(winner) < players.indexOf(looser))
+            return
+
+        players.remove(winner)
+        val indexOfLooser = players.indexOf(looser)
+        players.add(indexOfLooser, winner)
     }
 }
 
-interface Fight {
+interface Battle {
     fun fight(player1: Player, player2: Player): Player?
+}
+
+class InnerClanBattle : Battle {
+    override fun fight(player1: Player, player2: Player): Player? {
+        if (player1 == player2) return null
+        return player1
+    }
 }
